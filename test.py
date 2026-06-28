1906d572a76134a50d31e86fa30fe6735213c216
thanaphat06 <azaz2007azlove@gmail.com>
2026-06-28 15:31:52 +0700

    ava
    ava

--- test.py
+++ test.py
@@ -2,3 +2,19 @@ print("test jaa")
 print("test 2")
 print("888")
 print("test 3")
+
+n = int(input("Enter a number: "))
+if n%2 == 0:
+    print(f"{n} is even")
+else:
+    print(f"{n} is odd")
+
+x = int(input("Enter another number: "))
+for i in range(1, x + 1):
+    print(f"{i}",end=" ")
+
+cream = input("Enter your name : ")
+if cream =="cream":
+    print("your is cream")
+else:
+    print("sry your name is not cream")
